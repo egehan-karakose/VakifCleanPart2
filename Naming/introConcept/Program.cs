81e50775bbb2ceeeb859cbd4a0f81ce5db42df6a
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-11-22 11:27:01 +0300

    Naming

---
+++ Naming/introConcept/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace introConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int d = 0; //gün olarak geçen süre...
+            int elapsedTimeInDays = 0;
+            int daysSinceCreation = 0;
+            int gecenGunSayisi = 0;
+
+            int hitPoint = 0;
+
+            List<string> accounts = new List<string>();
+            List<string> accountList = new List<string>();
+
+
+            //İğrenç isimlendirme örnekleri
+            //Bunu yazan yazılımcı kör oldu :(
+            //var o = 0;
+            //var l = 1;
+            //var ol = 01;
+
+            //int a = 1;
+            //if (o==l)
+            //{
+            //    a = ol;
+            //}
+            //else
+            //{
+            //    l = ol;
+            //}
+          
+
+        }
+
+        private static List<int[]> cells = new List<int[]>();
+        static List<int[]> getFlaggedCells()
+        {
+            List<int[]> flaggedCells = new List<int[]>();
+            foreach (var cell in cells)
+            {
+                if (cell[0]==(int)FlagState.Flagged)
+                {
+                    flaggedCells.Add(cell);
+                }
+            }
+            return flaggedCells;
+        }
+        
+             
+    }
+
+    public enum FlagState { 
+        Flagged=4,
+        NonFlagged=0
+    }
+}
