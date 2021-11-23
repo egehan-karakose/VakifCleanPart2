a1ed9cadc1d5b3913671a7a2fdf5bd70f4d2b97c
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-11-23 11:22:51 +0300

    Last changing

---
+++ SOLID/DependencyInveresion/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DependencyInveresion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Bağımlılıklar -tersine çevrilebilir- bir biçimde tasarlanmalı.
+             * Büyük sistemler küçüklere DEĞİL küçük sistemler büyüklere bağlı olmalı!
+             *  Elektrik                        lamba 
+             */
+
+            MailSender mailSender = new MailSender();
+            ReportGenerator reportGenerator = new ReportGenerator(mailSender);
+            ReportGenerator wsReport = new ReportGenerator(new WhatsappSender());
+
+            reportGenerator.Send();
+            wsReport.Send();
+        }
+
+
+    }
+
+    public class ReportGenerator
+    {
+        ISender sender;
+        public ReportGenerator(ISender mailSender)
+        {
+            this.sender = mailSender;
+        }
+
+        public void Send()
+        {
+            sender.Send();
+        }
+    }
+
+    public interface ISender
+    {
+        void Send();
+    }
+    public class MailSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Mail gönderildi....");
+        }
+    }
+
+    public class WhatsappSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Whatsapp ile gönderildi");
+        }
+    }
+
+
+
+
+
+}
--- SOLID/InterfaceSegregation/Program.cs
+++ SOLID/InterfaceSegregation/Program.cs
@@ -12,5 +12,41 @@ namespace InterfaceSegregation
              * Bir sınıf, bir interface'i implemente ediyor fakat implemente edilen metotlardan sadece birkaçı nesne tarafından kullanılıyor.
              */
         }
+
+        public interface IMessage
+        {
+             string From { get; set; }
+             string To { get; set; }
+             string Subject { get; set; }
+            
+           
+        }
+
+        public interface IVideoMessage : IMessage
+        {
+            int VideoDuration { get; set; }
+            string VideoFormat { get; set; }
+        }
+
+        public interface IImageMessage: IMessage
+        {
+            int ImageSize { get; set; }
+        }
+
+        public class TextMessage : IMessage
+        {
+            public string From { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string To { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Subject { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        }
+
+        public class VideoMessage : IVideoMessage
+        {
+            public int VideoDuration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string VideoFormat { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string From { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string To { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Subject { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        }
     }
 }
