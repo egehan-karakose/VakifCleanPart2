e02299f25a98c710b9893855964c9d18f3d9fdd3
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-11-23 16:43:08 +0300

    Clean code result

---
+++ RefactoringToGoodCode/BrownField/TimeSheetProcessor.cs
@@ -0,0 +1,47 @@
+﻿using BrownField.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrownField
+{
+    public class TimeSheetProcessor
+    {
+        public double GetHoursWorkForCompany(List<TimeSheetEntry> timeSheetEntries, string companyName)
+        {
+            double totalHoursWorked = 0;
+            for (int i = 0; i < timeSheetEntries.Count; i++)
+            {
+                if (timeSheetEntries[i].WorkDone.ToLower().Contains(companyName))
+                {
+                    totalHoursWorked += timeSheetEntries[i].HoursWorked;
+                }
+            }
+
+            return totalHoursWorked;
+        }
+
+        public decimal CalcualateForWorkHours(List<TimeSheetEntry> timeSheets, Employee employee)
+        {
+            decimal paymentForWorkedHours = 0;
+            double totalWorkedHours = 0;
+
+            for (int i = 0; i < timeSheets.Count; i++)
+            {
+                totalWorkedHours += timeSheets[i].HoursWorked;
+            }
+            if (totalWorkedHours > 40)
+            {
+                paymentForWorkedHours = (decimal)(totalWorkedHours - 40) * (employee.HourlyRate*1.5M) + (40 * employee.HourlyRate);
+            }
+            else
+            {
+                paymentForWorkedHours = (decimal)totalWorkedHours * employee.HourlyRate;
+            }
+
+            return paymentForWorkedHours;
+        }
+    }
+}
---
+++ RefactoringToGoodCode/RefactoringToGoodCode/Program.cs
@@ -0,0 +1,83 @@
+﻿using BrownField;
+using BrownField.Models;
+using System;
+using System.Collections.Generic;
+
+namespace RefactoringToGoodCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee = new Employee { FirstName = "Türkay", HourlyRate = 20 };
+            
+            double totalWrokedHours;
+            List<TimeSheetEntry> timeSheets = LoadTimeSheets();
+            TimeSheetProcessor timeSheetProcessor = new TimeSheetProcessor();
+            totalWrokedHours = timeSheetProcessor.GetHoursWorkForCompany(timeSheets, "acme");           
+            BillInfo("Acme", 150, totalWrokedHours);
+            totalWrokedHours = timeSheetProcessor.GetHoursWorkForCompany(timeSheets, "abc");
+            BillInfo("ABC", 120, totalWrokedHours);
+
+            var payment = timeSheetProcessor.CalcualateForWorkHours(timeSheets,employee);
+
+            Console.WriteLine("You will get paid $" + payment + " for your time.");
+
+            Console.WriteLine();
+            Console.Write("Press any key to exit application...");
+            Console.ReadKey();
+        }
+
+        static void BillInfo(string companyName, decimal hourlyRate, double totalWorkedHours)
+        {
+            Console.WriteLine($"Simulating Sending email to {companyName} ");
+            Console.WriteLine("Your bill is $" + (decimal)totalWorkedHours * hourlyRate + " for the hours worked.");
+        }
+
+        private static List<TimeSheetEntry> LoadTimeSheets()
+        {
+            List<TimeSheetEntry> loadedTimeSheetEntries = new List<TimeSheetEntry>();
+            string answer = string.Empty;
+            do
+            {
+                Console.Write("Enter what you did: ");
+                string workDone = Console.ReadLine();
+                Console.Write("How long did you do it for: ");
+                string rawTimeWorked = Console.ReadLine();
+                double timeWorked = getDoubleFromValue(ref rawTimeWorked);
+                TimeSheetEntry timeSheetEntry = getTimeSheet(workDone, timeWorked);
+                loadedTimeSheetEntries.Add(timeSheetEntry);
+                Console.Write("Do you want to enter more time (yes/no): ");
+                answer = Console.ReadLine();
+            } while (answer.ToLower() == "yes");
+
+            return loadedTimeSheetEntries;
+        }
+
+        private static TimeSheetEntry getTimeSheet(string workDone, double timeWorked)
+        {
+            TimeSheetEntry timeSheetEntry = new TimeSheetEntry();
+            timeSheetEntry.HoursWorked = timeWorked;
+            timeSheetEntry.WorkDone = workDone;
+            return timeSheetEntry;
+        }
+
+        private static double getDoubleFromValue(ref string rawTimeWorked)
+        {
+            double timeWorked = 0;
+            while (double.TryParse(rawTimeWorked, out timeWorked) == false)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Invalid number given");
+                Console.Write("How long did you do it for: ");
+                rawTimeWorked = Console.ReadLine();
+            }
+
+            return timeWorked;
+        }
+    }
+
+
+}
+
